451bebdb8856b4a7ebd93091e90adec4fb209ed7
seewruuk <sewruczekk@icloud.com>
2020-03-18 01:31:42 +0100

    added Profile Page, EventList Page, EventList Page, Map Page

--- screens/eventList.js
+++ screens/eventList.js
@@ -10,7 +10,6 @@ function EventList({ navigation: { navigate, dispatch } }) {
     return (
         <View style={styles.mainContainer}>
             <View style={styles.headerContainer}>
-                <Image source={require('../assets/logo.png')} style={styles.logo}/>
                 <Text style={styles.headerText}>Wydarzenia</Text>
                 <TextInput
                     style={styles.inputText}
@@ -25,19 +24,85 @@ function EventList({ navigation: { navigate, dispatch } }) {
                     <Text style={styles.buttonText}>Filtruj</Text>
                 </TouchableOpacity>
                 <View style={styles.sortText}>
-                    <View style={styles.sortDate}>
-                        <Text>Data: </Text>
-                        <Text>w tym miesiącu</Text>
-                    </View>
-                    <View style={styles.sortDate}>
-                        <Text>Wiek: </Text>
-                        <Text>+18 lat</Text>
-                    </View>
+                        <Text>Data: w tym miesiącu</Text>
+                        <Text>Wiek: +18 lat</Text>
                 </View>
             </View>
             <View style={styles.contentContainer}>
                 <ScrollView style={styles.eventListContainer}>
+                    <View style={styles.orgWydContainer}>
+                        <View>
+                            <Text style={styles.paddingLeft}>Organizator</Text>
+                        </View>
+                        <View>
+                            <Text style={styles.paddingLeft}>Wydarzenie</Text>
+                        </View>
 
+                    </View>
+                    <TouchableOpacity>
+                    <View style={styles.eventListEventsContainer}>
+                        <Image source={require('../assets/hpclogo.png')} style={styles.logoProperties}/>
+                        <View style={styles.eventListEventInfoContainer}>
+                            <View style={styles.InformationsContent}>
+                                <Text style={styles.textProp}>Połowinki II LO Gdańsk</Text>
+                                <Text style={styles.textProp}>Cena Biletu: od 30zł</Text>
+                                <Text style={styles.textProp}>Miejsce: Zdarta Płytwa 33.5 Gdańsk</Text>
+                                <Text style={styles.textProp}>Zainteresowanych</Text>
+                            </View>
+                            <View style={styles.TextContent}>
+                                    <Text style={styles.eighteen}>18+</Text>
+                                </View>
+                        </View>
+                    </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <View style={styles.eventListEventsContainer}>
+                            <Image source={require('../assets/hpclogo.png')} style={styles.logoProperties}/>
+                            <View style={styles.eventListEventInfoContainer}>
+                                <View style={styles.InformationsContent}>
+                                    <Text style={styles.textProp}>Połowinki II LO Gdańsk</Text>
+                                    <Text style={styles.textProp}>Cena Biletu: od 30zł</Text>
+                                    <Text style={styles.textProp}>Miejsce: Zdarta Płytwa 33.5 Gdańsk</Text>
+                                    <Text style={styles.textProp}>Zainteresowanych</Text>
+                                </View>
+                                <View style={styles.TextContent}>
+                                    <Text style={styles.eighteen}>18+</Text>
+                                </View>
+                            </View>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <View style={styles.eventListEventsContainer}>
+                            <Image source={require('../assets/hpclogo.png')} style={styles.logoProperties}/>
+                            <View style={styles.eventListEventInfoContainer}>
+                                <View style={styles.InformationsContent}>
+                                    <Text style={styles.textProp}>Połowinki II LO Gdańsk</Text>
+                                    <Text style={styles.textProp}>Cena Biletu: od 30zł</Text>
+                                    <Text style={styles.textProp}>Miejsce: Zdarta Płytwa 33.5 Gdańsk</Text>
+                                    <Text style={styles.textProp}>Zainteresowanych</Text>
+                                </View>
+                                <View style={styles.TextContent}>
+                                    <Text style={styles.eighteen}>18+</Text>
+                                </View>
+                            </View>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <View style={styles.eventListEventsContainer}>
+                            <Image source={require('../assets/hpclogo.png')} style={styles.logoProperties}/>
+                            <View style={styles.eventListEventInfoContainer}>
+                                <View style={styles.InformationsContent}>
+                                    <Text style={styles.textProp}>Połowinki II LO Gdańsk</Text>
+                                    <Text style={styles.textProp}>Cena Biletu: od 30zł</Text>
+                                    <Text style={styles.textProp}>Miejsce: Zdarta Płytwa 33.5 Gdańsk</Text>
+                                    <Text style={styles.textProp}>Zainteresowanych</Text>
+                                </View>
+                                <View style={styles.TextContent}>
+                                    <Text style={styles.eighteen}>18+</Text>
+                                </View>
+                            </View>
+                        </View>
+                    </TouchableOpacity>
                 </ScrollView>
 
             </View>
@@ -55,17 +120,45 @@ const styles = StyleSheet.create({
         flex:2,
         alignItems: 'center',
     },
-    contentContainer:{
-        marginTop:30,
-        flex:4,
-        alignItems: 'center',
-    },
-    logo:{
-        height: 32,
-        width: 89,
-        resizeMode: 'contain',
-        alignItems: 'center',
+    eventListEventInfoContainer: {
+        flex: 1,
+        flexDirection: 'row',
         justifyContent: 'center',
+        alignItems: 'center',
+        padding: 10,
+    },
+    eighteen: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: "#963636",
+
+    },
+    TextContent: {
+        borderRadius: 10,
+        backgroundColor: "#fff",
+        paddingVertical: 15,
+        paddingHorizontal: 15,
+    },
+    textProp:{
+        fontSize: 10,
+    },
+    eventListEventsContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingHorizontal: 15,
+        paddingVertical: 10,
+    },
+
+    contentContainer:{
+        flex: 3.5,
+        alignItems: 'center',
+
+    },
+    logoProperties: {
+        width: 80,
+        height: 80,
+        borderRadius: 10,
     },
     headerText:{
         height: 26,
@@ -76,10 +169,14 @@ const styles = StyleSheet.create({
         marginTop: 16,
         fontSize: 20,
     },
+    paddingLeft: {
+        paddingLeft: '10%',
+        fontSize: 10,
+    },
     inputText:{
         height: 41,
         width: 256,
-        backgroundColor: '#f8e2e2',
+        backgroundColor: '#e0e0e0',
         borderRadius: 12,
         marginTop: 8,
     },
@@ -95,27 +192,24 @@ const styles = StyleSheet.create({
     buttonText:{
         color:'#fff',
     },
-    sortText:{
+
+    orgWydContainer: {
         marginTop: 12,
         width: 353,
         height: 24,
         flexDirection: 'row',
-        alignContent: 'space-between',
-        justifyContent: 'space-between'
     },
-    sortDate:{
-        flexDirection: 'row',
 
+    sortText:{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
     },
     sortAge:{
         flexDirection: 'row',
     },
     eventListContainer:{
-        height: 10,
-        width: 344,
         borderRadius: 12,
-        backgroundColor: '#fff0f0',
-        marginBottom: 10,
+        backgroundColor: '#e0e0e0',
     },
 });
 
